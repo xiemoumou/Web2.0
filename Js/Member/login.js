8c0e9aa55096a914459ef92d5f36614cc6a1f92c
liugang <example@qq.com>
2018-09-18 09:31:15 +0800

    处理登录与工作台来回跳的问题

--- Js/Member/login.js
+++ Js/Member/login.js
@@ -32,34 +32,20 @@ $(function () {
 
 
         }
-
-
     });
 
-    var pas = $.cookie('password');//记住密码自动登录
-    var token = $.cookie('token');//获取token
-    if (token!=undefined){
+    //自动登录
+    var phone=$.cookie("phone");
+    var password=$.cookie("password");
+    if(phone && password)
+    {
         $("#male").prop("checked",true);
         $("#auto-btn").removeClass('auto-btn').addClass('BtnActive');
-        $(".phone").val($.cookie("phone"));
-        $(".password").val($.cookie("password"));
-        var phone = $(".phone").val();
-        var PassWord = $(".password").val();
-        data = {
-            "mobilephone": phone,
-            "password": PassWord,
-        };
-        var url = config.WebService()["userAccountInfo_Query"];
-        Requst.ajaxGet(url,data,true,function (data) {
-            //$.cookie('token', data.data.token, {path: "/"});
-           // sessionStorage.setItem('token',data.data.token);
-            Helper.Cache.set('token',data.data.token,7);//token
-            window.location.href = '../workbench.html';
-
-        })
+        $(".phone").val(phone);
+        $(".password").val(password);
+        //调用登录
+        login.login(phone,password);
     }
-
-
 });
 
 
@@ -135,9 +121,6 @@ var mail = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
 
      },
      Keyup: function () {
-         // $(".modify-box input").keyup(function () {
-         //     $(this).val().replace(/^[\u4e00-\u9fa5 ]{1,20}$/, ' ');
-         // })
          $('.num_pass-box input').on('keyup change', function () {
              var that = $(this);
              var val = that.val();
@@ -155,47 +138,43 @@ var mail = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
          
          var phone = $(".phone").val();
          var PassWord = $(".password").val();
-         data = {
-             "mobilephone": phone,
-             "password": PassWord,
-         };
 
          if($('#male').prop('checked')) {
-             //存储一个带7天期限的cookie
-             //$.cookie("phone", phone, { expires: 7, path: '/' });
-             //$.cookie("password", PassWord, { expires: 7, path: '/' });
              Helper.Cache.set("phone", phone,7);
              Helper.Cache.set("password", PassWord,7);
          }
          else {
-            // $.cookie("phone", phone, { expires: 7, path: '/' });
-            // $.cookie("password", '', { expires: -1 , path: '/'});
              Helper.Cache.set("phone", phone,7);
              Helper.Cache.set("password", '',-1);
          }
+        //调用登录
+         login.login(phone,PassWord);
+     },
+     login:function (phone,PassWord) {
          var url = config.WebService()["userAccountInfo_Query"];
-
+         var data = {
+             "mobilephone": phone,
+             "password": PassWord,
+         };
          Requst.ajaxGet(url,data,true,function (data) {
 
-           if (data.code ==99999){
-               $(".btn-err").removeClass('hide');
-               return false;
-           } else if (data.code ==100001){
-               $(".user-err").removeClass('hide');
-               if (data.data.errorNum>10){
-                  Message.show('提示','你已经尝试以错误的密码登录系统达到10次，为了你的安全，将进行人机验证，如果您忘记了密码，请联系管理员重置密码后再次登录',MsgState.Warning,2000)
-               }
-               return false;
-           }
-           else{
-               //$.cookie('token', data.data.token, {path: "/"});
-               Helper.Cache.set('token',data.data.token,7);//token
-               Helper.Cache.set('mickName',data.data.mickName,7);//mickName
-               Helper.Cache.set('roleType',data.data.roleType,7);//roleType
-               Helper.Cache.set('userId',data.data.userId,7);//userId
-               window.location.href = '../workbench.html';
-           }
+             if (data.code ==99999){
+                 $(".btn-err").removeClass('hide');
+                 return false;
+             } else if (data.code ==100001){
+                 $(".user-err").removeClass('hide');
+                 if (data.data.errorNum>10){
+                     Message.show('提示','你已经尝试以错误的密码登录系统达到10次，为了你的安全，将进行人机验证，如果您忘记了密码，请联系管理员重置密码后再次登录',MsgState.Warning,2000)
+                 }
+                 return false;
+             }
+             else{
+                 Helper.Cache.set('token',data.data.token,7);//token
+                 Helper.Cache.set('mickName',data.data.mickName,7);//mickName
+                 Helper.Cache.set('roleType',data.data.roleType,7);//roleType
+                 Helper.Cache.set('userId',data.data.userId,7);//userId
+                 window.location.href = '../workbench.html';
+             }
          });
      }
-     
  }
